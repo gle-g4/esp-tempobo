21d2d1172eb9c31a5ea077072827afee8d0971aa
osouzajoao <69738438+JoaoSouza-ops@users.noreply.github.com>
2025-11-17 21:37:03 -0300

    Add Firebase configuration file
    This file contains sensitive Firebase configuration details.

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// IMPORTANTE: Este arquivo contém configurações sensíveis
+// NÃO compartilhe publicamente
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_98m8YaKIe3QHXtNH39B8TVGWTE-vI4U",
+    authDomain: "esp-projeto-5d4b2.firebaseapp.com",
+    databaseURL: "https://esp-projeto-5d4b2-default-rtdb.firebaseio.com",
+    projectId: "esp-projeto-5d4b2",
+    storageBucket: "esp-projeto-5d4b2.firebasestorage.app",
+    messagingSenderId: "1094472612847",
+    appId: "1:1094472612847:web:ea237814e34e519836141a"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
